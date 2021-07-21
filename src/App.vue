3c8da2cb590387c0049b08efe1842a92680e3ad4
jkriddle <josh@theriddlebrothers.com>
2021-07-21 11:14:12 -0400

    Background price retrieval

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,34 @@ export default {
   name: 'App',
   components: {
     Portfolio
+  },
+  data() {
+    return {
+      polling: null,
+      prices: null
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.polling);
+  },
+  created() {
+    this.pollData();
+  },
+  methods: {
+    getPrice() {
+      return 5;
+    },
+    pollData() {
+      this.polling = setInterval(() => {
+        //this.$store.dispatch('RETRIEVE_DATA_FROM_BACKEND');
+        fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_24hr_change=true')
+        .then(r => r.json())
+        .then((data) => {
+          this.prices = data;
+          console.log('data update');
+        });
+      }, 10000);
+    }
   }
 }
 </script>
